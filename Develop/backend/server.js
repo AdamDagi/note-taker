196c4c8a2ae17415e2cde044586e3c687f749b39
AdamDagi <mugutdinovshiha02@gmail.com>
2021-12-08 19:46:27 -0500

    test commit

--- Develop/backend/server.js
+++ Develop/backend/server.js
@@ -1,12 +1,20 @@
+// const { notes } = require('../public/assets/js/index');
+const html = require("")
 const express = require('express');
 const app = express();
-const port = 5500;
+const PORT = 3300;
+
+app.get('/', (reg, res) => {
+  console.log('string');
+  res.send('<h1>Hi there</h1>');
+});
 
 app.get('/notes', (req, res) => {
-  res.send('Hello World!')
+  let results = "test";
+  console.log('api notes');
+  res.json(results);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
